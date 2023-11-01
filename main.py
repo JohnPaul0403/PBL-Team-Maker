d76d13ce650a0b19a10db88e8cb5a6dfb9a59d79
JohnPaul0403 <johnpar2004@gmail.com>
2023-11-01 15:04:36 +0900

    Main File add

---
+++ main.py
@@ -0,0 +1,10 @@
+#Project Team Maker
+
+#This project is for making teams based on their score
+#The user will input the number of team members (n) it want as input
+
+#Libraries Import 
+
+#Main function, no variable declaration
+
+#Main function call
